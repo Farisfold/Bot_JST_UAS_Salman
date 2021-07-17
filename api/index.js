2f0e28cb13a2ac80edb0cb055ff1c50478f40f8c
Farisfold <salman7united@gmail.com>
2021-07-17 10:54:21 +0700

    1st Commit
    1st Commit

---
+++ api/index.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var r = express.Router();
+
+// load pre-trained model
+const model = require('./sdk/model.js');
+
+// Bot Setting
+const TelegramBot = require('node-telegram-bot-api');
+const token = '1822860103:AAEyvoRhuqJGpyhwIta7xgwWRojP4MJSlOQ'
+const bot = new TelegramBot(token, {polling: true});
+
+
+// bots
+bot.onText(/\/start/, (msg) => { 
+    console.log(msg)
+    bot.sendMessage(
+        msg.chat.id,
+        `hello ${msg.chat.first_name}, welcome...\n
+        Selamat datang di bot UAS islahuddin
+        click /predict`
+    );   
+});
+
+state = 0
+bot.onText(/\/predict/, (msg) => { 
+    bot.sendMessage(
+        msg.chat.id,
+        `input nilai x1|x2|x3 contohnya 27|21|81 (sesuai dengan nilai pada dataset)`
+    );   
+    state = 1;
+});
+
+bot.on('message', (msg) => {
+    if(state == 1){
+        s = msg.text.split("|");
+        x1 = s [0]
+        y1 = s [1]
+        model.predict(
+            [
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1]),
+                parseFloat(s[2])
+            ]
+        ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai y1 yang diprediksi adalah ${jres [0]} `
+                
+            ); 
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai y2 yang diprediksi adalah ${jres [1]} `
+            );
+            
+             bot.sendMessage(
+                msg.chat.id,
+                `nilai y3 yang diprediksi adalah ${jres [2]} `
+            );
+        })
+    }else{
+        state = 0
+    }
+})
+// routers
+r.get('/prediction/:x1/:x2/:x3', function(req, res, next) {    
+    model.predict(
+        [
+            parseFloat(req.params.x1), // string to float
+            parseFloat(req.params.x2),
+            parseFloat(req.params.x3)
+        ]
+    ).then((jres)=>{
+        res.json(jres);
+    })
+});
+
+module.exports = r;
---
+++ api/sdk/model.js
@@ -0,0 +1,44 @@
+const tf = require('@tensorflow/tfjs-node');
+
+function normalized(data){ // x1, x2, x3
+    x1 = (data[0] - 42.773) / 10.33017006
+    x2 = (data[1] - 29.9412) / 8.936247191
+    x3 = (data[2] - 94.8964) / 8.887376673
+    return [x1, x2, x3]
+}
+
+function denormalized(data){ // y1, y2, y3
+    y1 = (data[0] * 16.08197907) + 32.2718
+    y2 = (data[1] * 8.918185118) + 39.959
+    y3 = (data[2] * 11.79955613) + 69.739
+    return [y1, y2, y3]
+}
+
+
+async function predict(data){
+    let in_dim = 3;
+    
+    data = normalized(data);
+    shape = [1, in_dim];
+
+    tf_data = tf.tensor2d(data, shape);
+
+    try{
+        // path load in public access => github
+        const path = 'https://raw.githubusercontent.com/Islahuddin41420110058/UASKEDUA/main/api/sdk/model/model%20(1).json';
+        const model = await tf.loadGraphModel(path);
+        
+        predict = model.predict(
+                tf_data
+        );
+        result = predict.dataSync();
+        return denormalized( result );
+        
+    }catch(e){
+      console.log(e);
+    }
+}
+
+module.exports = {
+    predict: predict 
+}
